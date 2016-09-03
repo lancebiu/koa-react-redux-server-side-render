303089561abbc5c82ec6dcf23303fb4c52ed0791
lance <luohx1121@gmail.com>
2016-09-04 02:31:49 +0800

    add controller

--- app/server.js
+++ app/server.js
@@ -6,20 +6,11 @@ import staticCache from 'koa-static-cache';
 import render from 'koa-ejs';
 import route from 'koa-route';
 import path from 'path';
-import fs from 'fs';
-import thunkify from 'thunkify';
-import React from 'react';
-import ReactDOMServer from 'react-dom/server';
-import {match, RouterContext} from 'react-router';
-import routes from './routes';
-import {createStore, applyMiddleware} from 'redux';
-import thunkMiddleware from 'redux-thunk';
-import {Provider} from 'react-redux';
-import rootReducer from './redux/rootReducer';
+
+import siteController from './controller/site';
+import billController from './controller/bill';
 
 const app = koa();
-
-const readFile = thunkify(fs.readFile);
 
 // static file serving
 app.use(staticCache('./app/public', {
@@ -34,61 +25,9 @@ render(app, {
     debug: true
 });
 
-const reactView = function *() {
-
-    const [redirectLocation, renderProps] = yield thunkify(match)({routes, location: this.request.url});
-
-    const store = applyMiddleware(
-        thunkMiddleware
-    )(createStore)(rootReducer, {});
-
-    const components = renderProps.components.filter(
-        component => typeof component.fetchData === 'function'
-    );
-
-    const promises = components.map(component =>
-        component.fetchData({
-            dispatch:store.dispatch, params: renderProps.params
-        })
-    )
-
-    yield Promise.all(promises);
-
-    yield new Promise((resolve, reject) =>
-        setTimeout(() => {
-            resolve();
-        }, 0)
-    )
-
-    const html = ReactDOMServer.renderToString(
-        <Provider store={store}>
-            <RouterContext {...renderProps}/>
-        </Provider>
-    );
-
-    yield this.render('index', {
-        content: html,
-        state: JSON.stringify(store.getState())
-    });
-
-};
-
-const latestBills = function *() {
-    var data = yield readFile('./data/latest-bills.json');
-    this.type = 'json';
-    this.body = JSON.parse(data);
-}
-
-const billInfo = function *(billId) {
-    var data = yield readFile('./data/detailed-bills.json');
-    var billData = JSON.parse(data).items.filter(item => item.id === billId)[0];
-    this.type = 'json';
-    this.body = billData;
-};
-
-app.use(route.get('/api/bills', latestBills));
-app.use(route.get('/api/bill/:id', billInfo));
-app.use(route.get('*', reactView));
+app.use(route.get('/api/bills', billController.list));
+app.use(route.get('/api/bill/:id', billController.info));
+app.use(route.get('*',siteController.home));
 
 app.listen(3000, function () {
     console.log('Server listening at port 3000...');
